22432a5a83466c5f6766c561d9583e9404d0db16
Abetik <abetik.dev@gmail.com>
2026-04-27 22:28:38 +0300

    fix

--- src/modules/applications/dto/review-application.dto.ts
+++ src/modules/applications/dto/review-application.dto.ts
@@ -1,6 +1,11 @@
-import {ApplicationStatus} from "../entities/organizer-application.entity";
-
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { ApplicationStatus } from "../entities/organizer-application.entity";
 
 export class ReviewApplicationDto {
+    @IsEnum(ApplicationStatus)
     status: ApplicationStatus;
+
+    @IsOptional()
+    @IsString()
+    adminComment?: string;
 }
